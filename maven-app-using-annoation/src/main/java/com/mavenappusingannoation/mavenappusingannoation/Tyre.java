fcf246b9b0a9c8e26920cbc8f325971f8672d750
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-06-30 12:57:24 +0530

    constructor injection

--- maven-app-using-annoation/src/main/java/com/mavenappusingannoation/mavenappusingannoation/Tyre.java
+++ maven-app-using-annoation/src/main/java/com/mavenappusingannoation/mavenappusingannoation/Tyre.java
@@ -1,20 +1,33 @@
 package com.mavenappusingannoation.mavenappusingannoation;
 
 public class Tyre {
-    private String brand;
-
-    public String getBrand() {
-        return brand;
+    private String brand,companyType;
+    Tyre(String brand,String companyType)
+    {
+        super();
+        this.brand=brand;
+        this.companyType=companyType;
     }
+//    public String getBrand() {
+//        return brand;
+//    }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+//    public void setBrand(String brand) {
+//        this.brand = brand;
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "Tyre{" +
+//                "brand='" + brand + '\'' +
+//                '}'+" Company";
+//    }
 
     @Override
     public String toString() {
         return "Tyre{" +
                 "brand='" + brand + '\'' +
+                ", companyType='" + companyType + '\'' +
                 '}';
     }
 }
