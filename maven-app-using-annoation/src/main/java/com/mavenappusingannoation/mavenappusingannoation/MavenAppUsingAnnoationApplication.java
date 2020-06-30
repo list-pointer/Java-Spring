c87aec36fbecad5a3e1f1f35af0b89461c0afe17
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-06-30 11:43:32 +0530

    Initalized Basic MavenApp using annonation technique

---
+++ maven-app-using-annoation/src/main/java/com/mavenappusingannoation/mavenappusingannoation/MavenAppUsingAnnoationApplication.java
@@ -0,0 +1,13 @@
+package com.mavenappusingannoation.mavenappusingannoation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAppUsingAnnoationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenAppUsingAnnoationApplication.class, args);
+    }
+
+}
---
+++ maven-app-using-annoation/src/test/java/com/mavenappusingannoation/mavenappusingannoation/MavenAppUsingAnnoationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavenappusingannoation.mavenappusingannoation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAppUsingAnnoationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
