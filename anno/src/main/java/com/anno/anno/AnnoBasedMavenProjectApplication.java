9240bdb7fdad4d5aaa7dc8201a3c4617f65ec515
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-06-30 17:16:18 +0530

    annotation based app initalized

---
+++ anno/src/main/java/com/anno/anno/AnnoBasedMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.anno.anno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnoBasedMavenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnoBasedMavenProjectApplication.class, args);
+    }
+
+}
