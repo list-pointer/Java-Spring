af5d97e0716ddf2d616f4c5f46a86f8103bea9f5
Abhishek Gupta <guptaabhishek891@gmail.com>
2020-06-30 12:45:28 +0530

    add property using xml style

--- maven-app-using-annoation/src/main/java/com/mavenappusingannoation/mavenappusingannoation/MavenAppUsingAnnoationApplication.java
+++ maven-app-using-annoation/src/main/java/com/mavenappusingannoation/mavenappusingannoation/MavenAppUsingAnnoationApplication.java
@@ -11,8 +11,10 @@ public class MavenAppUsingAnnoationApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(MavenAppUsingAnnoationApplication.class, args);
         ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-        Vehicle obj=(Vehicle)context.getBean("car");
-        obj.drive();
+//        Vehicle obj=(Vehicle)context.getBean("car");
+//        obj.drive();
+        Tyre t=(Tyre)context.getBean("tyre");
+        System.out.println(t);
     }
 
 }
